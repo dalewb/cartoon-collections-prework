0670fe59223d9142acf1b5f625d8c09c4d1dd0a9
William Dale <williambdale@gmail.com>
2018-03-06 00:03:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(calls)
-  calls.collect {|el| el.capitalize << "!"
+  calls.collect {|el| el.capitalize << "!"}
 end
 
 summon_captain_planet(calls)
