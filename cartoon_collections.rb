2ccd0b5ebd738c1e71ea06e07155b00da81a2059
William Dale <williambdale@gmail.com>
2018-03-06 00:06:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,13 +8,15 @@ def summon_captain_planet(calls)
   calls.collect {|el| el.capitalize << "!"}
 end
 
-summon_captain_planet(calls)
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    if call.length > 4
+      return true 
+    end 
+  end 
+  false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
